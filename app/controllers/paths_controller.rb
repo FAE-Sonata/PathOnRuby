04dcef4b43c76517f2061ef2a4e059ca6cf7d103
FAE-Sonata <HeYu1993@gmail.com>
2021-10-17 15:47:33 -0400

    accessing by ID (R for Read); for full titles

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -2,4 +2,8 @@ class PathsController < ApplicationController
   def index
     @paths = Path.all
   end
+
+  def show
+    @path = Path.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "paths#index"
 
-  get "/paths", to: "paths#index"
+  resources :paths
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
