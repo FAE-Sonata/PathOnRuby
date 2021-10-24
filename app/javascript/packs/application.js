e3c0f92cd7e4a864a384dafd30cffbbd8537db9c
FAE-Sonata <HeYu1993@gmail.com>
2021-10-24 18:32:24 -0400

    initial bootstrap implementation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-require("../custom/addFields")
-require("../custom/removeFields")
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import 'bootstrap'; 
+// require("../custom/addFields");
+// require("../custom/removeFields");
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
